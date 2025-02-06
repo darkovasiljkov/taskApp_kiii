e9b522e16cef21394ce1404f44f17dd65ed846c0
Darko <93830641+darkovasiljkov@users.noreply.github.com>
2025-02-06 02:29:45 +0100

    Initial commit for the app

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,6 @@ app.config["SECRET_KEY"] = "2c5e068f60b7dbb1406874bccaf824efc5f34ffe"
 app.config["MONGO_URI"] = "mongodb+srv://vasiljkovdarko:codewithvasiljkovdarko@cluster0.upiu5.mongodb.net/cluster0"
 
 
-# username:vasiljkovdarko pw:9Y0tJOdWIq2wLvWb Cluster0
 # mongodb database
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
